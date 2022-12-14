25516f195890a99c304f66c3e204ddb8bf76cdd1
Harry Sudana <114111567+harrysudanaa@users.noreply.github.com>
2022-12-14 14:25:39 +0800

    Add files via upload

---
+++ SIAK_21IKI/src/id/ac/undiksha/siak/Main.java
@@ -0,0 +1,25 @@
+package id.ac.undiksha.siak;
+
+import id.ac.undiksha.siak.entities.Lecturer;
+import id.ac.undiksha.siak.entities.Student;
+import id.ac.undiksha.siak.entities.Staff;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		Student std1 = new Student();
+//		std1.setName("Jack Kahuna Lagoona");
+//		std1.setAddress("Bikini Bottom");
+//		System.out.print(std1.getAddress());
+//		System.out.print(std1.getName());
+//		std1.printAllInfo();
+
+//		Student std2 = new Student("121134", "Lucky", "New York", true, "Computer Science", "Informatics", "FTK");
+//		std2.printAllInfo();
+//		Lecturer l2 = new Lecturer("19951206", "Lucky", "New York", true, "Computer Science", "Informatics", "FTK");
+//		l2.printAllInfo();
+		Staff s2 = new Staff("19951206", "Lucky", "New York", true, "Language Unit");
+		s2.printAllInfo();
+	}
+
+}
